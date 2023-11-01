1d568de89c0708911cb363cb525b4882208d2d39
SathishKumarsss <138762223+SathishKumarsss@users.noreply.github.com>
2023-11-01 14:44:05 +0530

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,12 +1,3 @@
---
--- Database: `blog_eg`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `registered_users`
---
 
 CREATE TABLE `registered_users` (
   `id` int(8) NOT NULL,
@@ -18,29 +9,14 @@ CREATE TABLE `registered_users` (
   `about` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `registered_users`
---
 
 INSERT INTO `registered_users` (`id`, `user_name`, `display_name`, `password`, `email`, `photo`, `about`) VALUES
 (1, 'kate_91', 'Kate Winslet', '$2y$10$LVISX0lCiIsQU1vUX/dAGunHTRhXmpcpiuU7G7.1lbnvhPSg7exmW', 'kate@wince.com', 'images/photo.jpeg', 'Web developer');
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `registered_users`
---
 ALTER TABLE `registered_users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `registered_users`
---
 ALTER TABLE `registered_users`
   MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
